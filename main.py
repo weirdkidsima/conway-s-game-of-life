1977189c15f466819d9c47360819cad871e3102c
Ступников Максим <61547155+weirdkidsima@users.noreply.github.com>
2023-01-13 12:34:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame as pg
 from random import randint
 from copy import deepcopy
+from tkinter import messagebox as mb
 
 RESOLUTION = WIDTH, HEIGHT = 1280, 720 
 CELL = 10
@@ -15,14 +16,17 @@ surface = pg.display.set_mode(RESOLUTION)
 clock = pg.time.Clock()
 
 nextField = [[0 for i in range(WQTY)] for j in range(HQTY)]
-#VARIATIONS
-# existField = [[0 for i in range(WQTY)] for j in range(HQTY)]
-# existField = [[1 if i == WQTY // 2 or j == HQTY // 2 else 0 for i in range(WQTY)] for j in range(HQTY)]
-# existField = [[randint(0, 1) for i in range(WQTY)] for j in range(HQTY)]
-# existField = [[1 if not i % 9 else 0 for i in range(WQTY)] for j in range(HQTY)]
-# existField = [[1 if not (2 * i + j) % 4 else 0 for i in range(WQTY)] for j in range(HQTY)]
-# existField = [[1 if not (i * j) % 22 else 0 for i in range(WQTY)] for j in range(HQTY)]
-existField = [[1 if not i % 7 else randint(0, 1) for i in range(WQTY)] for j in range(HQTY)] #use one of the variations
+
+# RANDOM VARIATION
+variations = [
+    [[1 if i == WQTY // 2 or j == HQTY // 2 else 0 for i in range(WQTY)] for j in range(HQTY)],
+    [[randint(0, 1) for i in range(WQTY)] for j in range(HQTY)],
+    [[1 if not i % 9 else 0 for i in range(WQTY)] for j in range(HQTY)],
+    [[1 if not (2 * i + j) % 4 else 0 for i in range(WQTY)] for j in range(HQTY)],
+    [[1 if not (i * j) % 22 else 0 for i in range(WQTY)] for j in range(HQTY)],
+    [[1 if not i % 7 else randint(0, 1) for i in range(WQTY)] for j in range(HQTY)]
+]
+existField = variations[randint(0,len(variations) - 1)]
 
 def check_cell(existField, x, y):
     count = 0
@@ -48,6 +52,12 @@ while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
+        keys = pg.key.get_pressed()
+        if keys[pg.K_i]:
+            mb.showinfo(
+                "Информация",
+                "Создал Ступников М.А 22ИП2Б"
+            )
 
     # DRAW  CELLS
     for x in range(1, WQTY - 1):
@@ -58,6 +68,6 @@ while True:
 
     existField = deepcopy(nextField)
 
-    print(clock.get_fps())
+    # print(clock.get_fps())
     pg.display.flip()
     clock.tick(FPS)
